ef644614cde6a3ec6033493c4a809f59d271e22b
Andrey Panfilov <andreipanf@gmail.com>
2015-08-19 22:16:27 +0400

    Initial Commit

---
+++ WerbaryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WerbaryTest
+//
+//  Created by Andrey on 19.08.15.
+//  Copyright (c) 2015 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WerbaryTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WerbaryTest
+//
+//  Created by Andrey on 19.08.15.
+//  Copyright (c) 2015 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
